f51934d9347f853a4433801f99fe143e42d2c5dd
Doug Partridge <dpartridge@pivotmaritime.com.au>
2024-09-16 12:43:41 +1000

    retry connecting to mqtt broker

--- mqtt-test/mqtt-test.cpp
+++ mqtt-test/mqtt-test.cpp
@@ -50,7 +50,7 @@ shelly-pivot-1/command/input:0 = status_update
 // mosquitto
 
 
-const std::string mqtt_broker_ip = "192.168.88.158";
+const std::string mqtt_broker_address = "192.168.88.158";
 const int mqtt_broker_port = 1883;
 
 
@@ -80,11 +80,15 @@ int main()
 	mosquitto* mosq = mosquitto_new(nullptr, true, nullptr);
 
 
-
-	if (mosquitto_connect(mosq, mqtt_broker_ip.c_str(), mqtt_broker_port, 60) != MOSQ_ERR_SUCCESS)
-	{
+	while (true) {
+		if (mosquitto_connect(mosq, mqtt_broker_address.c_str(), mqtt_broker_port, 60) == MOSQ_ERR_SUCCESS)
+		{
+			break;
+		}
 		std::cerr << "Error: connecting to MQTT broker failed" << "\n";
+		std::this_thread::sleep_for(std::chrono::seconds(1));
 	}
+
 
 
 	mosquitto_message_callback_set(mosq, [](mosquitto* mosq, void* obj, const mosquitto_message* message) {
@@ -107,7 +111,7 @@ int main()
 
 
 	while (true) {
-		
+
 		pub(mosq, "shelly-pivot-1/command/switch:0", "toggle");
 
 
@@ -124,62 +128,3 @@ int main()
 
 
 
-// -----------------------------
-// paho-mqttpp3
-// 
-//#include "mqtt/client.h"
-//
-//
-//const int qos_at_most_once = 0;
-//const int qos_at_least_once = 1;
-//const int qos_exactly_once = 2;
-//
-//
-//int main()
-//{
-//    std::cout << "Hello World!" << "\n";
-//
-//
-//    const std::string server_address = "192.168.88.158";
-//    const std::string client_id = "test-client-paho";
-//
-//    mqtt::client cli(server_address, client_id);
-//
-//    mqtt::connect_options connOpts;
-//    connOpts.set_keep_alive_interval(20);
-//    connOpts.set_clean_session(true);
-//
-//    try {
-//        // Connect to the client
-//
-//        cli.connect(connOpts);
-//
-//        // Publish using a message pointer.
-//
-//        //auto msg = mqtt::make_message("shelly-pivot-1/command/switch:0", "off");
-//        //msg->set_qos(qos_at_least_once);
-//        //
-//        //cli.publish(msg);
-//
-//        // Now try with itemized publish.
-//
-//        //auto msg = mqtt::make_message( , "off");
-//
-//        std::string topic = "shelly-pivot-1/command/switch:0";
-//        std::string payload = "on";
-//
-//        cli.publish(topic, payload.data(), payload.size());
-//        //cli.publish(TOPIC, PAYLOAD2, strlen(PAYLOAD2), 0, false);
-//
-//        // Disconnect
-//
-//        cli.disconnect();
-//    }
-//    catch (const mqtt::exception& exc) {
-//        std::cerr << "Error: " << exc.what() << " ["
-//            << exc.get_reason_code() << "]" << std::endl;
-//        return 1;
-//    }
-//
-//    return 0;
-//}
